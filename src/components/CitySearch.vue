7ff98437c0d344a8515cb7702eae87e242284ae2
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2025-02-25 21:17:55 +0000

    Corrected search code

--- src/components/CitySearch.vue
+++ src/components/CitySearch.vue
@@ -2,13 +2,13 @@
   <div class="flex flex-col sm:flex-row items-center mb-8 space-y-4 sm:space-y-0 sm:space-x-4">
     <input
       v-model="searchCity"
-      @keyup.enter="getGeocodingResults"
+      @keyup.enter="emitGeocodingResults"
       type="text"
       placeholder="Enter city name"
       class="shadow-sm appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-white dark:border-gray-600"
     />
     <button
-      @click="getGeocodingResults"
+      @click="emitGeocodingResults"
       class="bg-orange-accent hover:bg-orange-500 text-white font-bold py-3 px-5 rounded focus:outline-none focus:shadow-outline transition-colors duration-300"
     >
       {{ $t('search') }}
@@ -17,21 +17,21 @@
 </template>
 
 <script>
-import { ref, defineEmits, inject } from 'vue';
+import { ref, inject } from 'vue';
 
 export default {
-  setup() {
+  emits: ['get-geocoding-results'],
+  setup(props, { emit }) {
     const searchCity = ref('');
-    const emit = defineEmits(['get-geocoding-results']);
     const t = inject('t');
 
-    const getGeocodingResults = () => {
+    const emitGeocodingResults = () => {
       emit('get-geocoding-results', searchCity.value);
     };
 
     return {
       searchCity,
-      getGeocodingResults,
+      emitGeocodingResults,
       t
     };
   }
